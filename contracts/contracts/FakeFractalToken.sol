c7e735a593ffacf9576cf19a5aaaab4240f92a5a
Miguel Palhas <mpalhas@gmail.com>
2021-04-01 15:39:47 +0100

    Cleaning up some things

--- contracts/contracts/FakeFractalToken.sol
+++ contracts/contracts/FakeFractalToken.sol
@@ -1,7 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.3;
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FakeFractalToken is ERC20 {
-  constructor(address targetOwner) public ERC20("Fractal Protocol Token", "FCL") {
+  constructor(address targetOwner) ERC20("Fractal Protocol Token", "FCL") {
     _mint(targetOwner, 465000000000000000000000000);
   }
 }
