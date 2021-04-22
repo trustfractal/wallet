a28104c5137f688d5d8d0d6a441a90c3e6a5af4e
Fernando Mendes <fernando@mendes.codes>
2021-04-22 17:49:12 +0100

    Include path when importing a mnemonic

--- backend/src/wallet/index.ts
+++ backend/src/wallet/index.ts
@@ -1,9 +1,11 @@
 import { Wallet } from "ethers";
 
-const mnemonic = process.env["WALLET_MNEMONIC"];
+const mnemonic = process.env["BACKEND_WALLET_MNEMONIC"];
+const path = process.env["BACKEND_WALLET_PATH"];
 
-if (!mnemonic) throw new Error("Missing WALLET_MNEMONIC env variable");
+if (!mnemonic) throw new Error("Missing BACKEND_WALLET_MNEMONIC env variable");
+if (!path) throw new Error("Missing BACKEND_WALLET_PATH env variable");
 
-const wallet = Wallet.fromMnemonic(mnemonic);
+const wallet = Wallet.fromMnemonic(mnemonic, path);
 
 export default wallet;
