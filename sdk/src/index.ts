146290bb7281ac7abe61d094facf382cd93d850e
Fernando Mendes <fernando@mendes.codes>
2021-04-27 19:00:21 +0100

    Add smart contract connection to the SDK

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -3,15 +3,17 @@ import Claim from "./Claim";
 import ClaimType from "./ClaimType";
 import Credential from "./Credential";
 import Crypto from "./Crypto";
+import DIDContract from "./DIDContract";
 import FractalError from "./FractalError";
 import Utils from "./utils";
 
 export {
-  Utils,
   AttestationRequest,
   Claim,
   ClaimType,
   Credential,
   Crypto,
+  DIDContract,
   FractalError,
+  Utils,
 };
