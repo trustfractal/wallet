871fdce2f8c6c639a1c7dc790a2dff2044826724
Fernando Mendes <fernando@mendes.codes>
2021-04-28 20:40:44 +0100

    Add backend as a submodule

--- backend/src/wallet/index.ts
+++
@@ -1,9 +0,0 @@
-import { Wallet } from "ethers";
-import { getEnv } from "../utils";
-
-const mnemonic = getEnv("BACKEND_WALLET_MNEMONIC");
-const path = getEnv("BACKEND_WALLET_PATH");
-
-const wallet = Wallet.fromMnemonic(mnemonic, path);
-
-export default wallet;
