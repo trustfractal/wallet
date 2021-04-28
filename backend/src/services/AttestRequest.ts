4eeeb13423e773e09cbbf537ae2cb2612e52a51e
Fernando Mendes <fernando@mendes.codes>
2021-04-28 15:46:36 +0100

    Sign credentials through the smart contract

--- backend/src/services/AttestRequest.ts
+++ backend/src/services/AttestRequest.ts
@@ -1,5 +1,27 @@
-import { AttestationRequest, Credential } from "@fractalwallet/sdk";
+import {
+  AttestationRequest,
+  DIDContract,
+  Credential,
+} from "@fractalwallet/sdk";
+
 import wallet from "../wallet";
+import { getEnv } from "../utils";
+
+const ETHEREUM_RPC_URL = getEnv("ETHEREUM_RPC_URL");
+const ETHEREUM_NETWORK = getEnv("ETHEREUM_NETWORK");
+
+const addContractSignature = async (credential: Credential) => {
+  console.log(ETHEREUM_RPC_URL);
+  const contract = new DIDContract(ETHEREUM_NETWORK, {
+    providerUrl: ETHEREUM_RPC_URL,
+  });
+
+  const credentialHash = await contract.computeSignableKey(credential);
+  const credentialSignature = await wallet.signMessage(credentialHash);
+
+  credential.credentialHash = credentialHash;
+  credential.credentialSignature = credentialSignature;
+};
 
 const addSignature = async (credential: Credential) => {
   const attesterSignature = await wallet.signMessage(credential.rootHash);
@@ -12,9 +34,12 @@ const perform = async (
   request: AttestationRequest
 ): Promise<Credential | null> => {
   const credential = Credential.fromRequest(request);
+
   await addSignature(credential);
 
   if (!credential.verifyIntegrity()) return null;
+
+  await addContractSignature(credential);
 
   return credential;
 };
