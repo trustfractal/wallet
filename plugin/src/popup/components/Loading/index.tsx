d961fe99ab408f7605aee0000839705db026df75
Diogo Couto <diogo2couto@gmail.com>
2021-05-05 22:08:24 +0100

    Add loading screen

--- plugin/src/popup/components/Loading/index.tsx
+++ plugin/src/popup/components/Loading/index.tsx
@@ -1,7 +1,50 @@
-import TopComponent from "@popup/components/common/TopComponent";
+import styled from "styled-components";
+
+import Logo from "@popup/components/common/Logo";
+import Spinner from "@popup/components/common/Spinner";
+import Text, {
+  TextHeights,
+  TextSizes,
+  TextWeights,
+} from "@popup/components/common/Text";
+
+const RootContainer = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 460px;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: var(--s-16);
+`;
+
+const SpinnerContainer = styled.div`
+  margin-right: var(--s-8);
+`;
 
 function Loading() {
-  return <TopComponent />;
+  return (
+    <RootContainer>
+      <Logo />
+      <TextContainer>
+        <SpinnerContainer>
+          <Spinner alternative />
+        </SpinnerContainer>
+        <Text
+          size={TextSizes.LARGE}
+          height={TextHeights.LARGE}
+          weight={TextWeights.BOLD}
+        >
+          Loading...
+        </Text>
+      </TextContainer>
+    </RootContainer>
+  );
 }
 
 export default Loading;
