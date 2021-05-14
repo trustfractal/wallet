acb1e157a277afb3ce89c58d930c9243445f693f
Diogo Couto <diogo2couto@gmail.com>
2021-05-14 16:19:38 +0100

    Fix checkbox and radio design

--- src/popup/components/common/CheckboxInput/index.tsx
+++ src/popup/components/common/CheckboxInput/index.tsx
@@ -2,27 +2,66 @@ import React from "react";
 
 import styled from "styled-components";
 
-const RootContainer = styled.div``;
+const CheckboxIcon = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
 
-const InputContainer = styled.input`
-  width: var(--s-24);
-  height: var(--s-24);
+  border-radius: 4px;
+
+  width: calc(var(--s-24) - 1px);
+  height: calc(var(--s-24) - 1px);
   border: 1px solid var(--c-dark-blue);
-  outline: none;
-  cursor: pointer;
 
-  :checked {
+  background-color: var(--c-white);
+
+  :after {
+    content: "";
+    display: block;
+    position: absolute;
+    content: "✓";
+    position: absolute;
+    color: var(--c-white);
+    font-weight: bold;
+    left: 4px;
+    top: 2px;
+  }
+`;
+
+const Checkbox = styled.input`
+  position: absolute;
+  opacity: 0;
+  height: 0;
+  width: 0;
+
+  :checked ~ ${CheckboxIcon} {
     background-color: var(--c-orange);
     border: 1px solid var(--c-white);
+  }
+`;
+
+const RootContainer = styled.div`
+  user-select: none;
+  cursor: pointer;
+  position: relative;
+
+  width: var(--s-24);
+  height: var(--s-24);
+
+  :hover ${Checkbox}:not(:checked) ~ ${CheckboxIcon} {
+    background-color: var(--c-dark-gray);
   }
 `;
 
 function CheckboxInput(props: React.InputHTMLAttributes<HTMLInputElement>) {
   const { children, ...otherProps } = props;
 
+  const ref = React.createRef<HTMLInputElement>();
+
   return (
     <RootContainer>
-      <InputContainer type="checkbox" {...otherProps} />
+      <Checkbox ref={ref} type="checkbox" {...otherProps} />
+      <CheckboxIcon onClick={() => ref.current?.click()} />
     </RootContainer>
   );
 }
