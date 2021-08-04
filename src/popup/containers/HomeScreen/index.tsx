164e9261a9f1455ea14392cfb443b947cf752397
Fernando Mendes <fernando@mendes.codes>
2021-08-04 16:27:56 +0100

    Load config from maguro
    Why:
    
    * We want the config coming from the protocol being enabled or not to
      come from Maguro.
    * This allows us to control when the testnet becomes live and available
      for everyone, instead of relying on an approval from Google and users
      to update accordingly.

--- src/popup/containers/HomeScreen/index.tsx
+++ src/popup/containers/HomeScreen/index.tsx
@@ -1,16 +1,18 @@
-import { useUserSelector } from "@redux/stores/user/context";
-
 import Tabs from "@popup/components/common/Tabs";
 import Credentials from "@popup/components/Credentials";
 import Protocol from "@popup/components/Protocol";
 import { withNavBar } from "@popup/components/common/NavBar";
 
+import { useAppSelector } from "@redux/stores/application/context";
+import { getProtocolEnabled } from "@redux/stores/application/reducers/app/selectors";
+import { useUserSelector } from "@redux/stores/user/context";
 import { getCredentials } from "@redux/stores/user/reducers/credentials/selectors";
 import { getRequests } from "@redux/stores/user/reducers/requests/selectors";
 
 function Home() {
   const requests = useUserSelector(getRequests);
   const credentials = useUserSelector(getCredentials);
+  const protocolEnabled = useAppSelector(getProtocolEnabled);
 
   const tabs = [
     {
@@ -24,6 +26,7 @@ function Home() {
       label: "Protocol",
       props: {},
       component: Protocol,
+      disabled: !protocolEnabled,
     },
   ];
 
--- src/redux/stores/application/aliases/AppAliases/index.js
+++ src/redux/stores/application/aliases/AppAliases/index.js
@@ -1,6 +1,7 @@
 import appActions, { appTypes } from "@redux/stores/application/reducers/app";
 
 import CredentialsPolling from "@models/Polling/CredentialsPolling";
+import MaguroService from "@services/MaguroService";
 
 export const startup = () => {
   return async (dispatch) => {
@@ -11,7 +12,11 @@ export const startup = () => {
     // eslint-disable-next-line no-undef
     const { version } = chrome.runtime.getManifest();
 
+    const { protocol_enabled: protocolEnabled } =
+      await MaguroService.getConfig();
+
     dispatch(appActions.setVersion(version));
+    dispatch(appActions.setProtocolEnabled(protocolEnabled));
     dispatch(appActions.setLaunched(true));
   };
 };
