5ca6104d2378a790f3958f87c9d80d673b922398
Shelby Doolittle <shelby@shelbyd.com>
2021-08-12 15:53:27 -0400

    register for minting regularly

--- src/scripts/background/connection/callbacks/ProtocolCallbacks.ts
+++ src/scripts/background/connection/callbacks/ProtocolCallbacks.ts
@@ -1,6 +1,8 @@
 import ConnectionTypes from "@models/Connection/types";
 
 import { DataHost } from "@services/DataHost";
+import storageService from '@services/StorageService';
+import {MintingRegistrar} from '@services/MintingRegistrar';
 
 export async function addWebpage([url]: [string]): Promise<void> {
   await DataHost.instance().storeFact({
@@ -9,6 +11,7 @@ export async function addWebpage([url]: [string]): Promise<void> {
       timestampMs: new Date().getTime(),
     },
   });
+  await (new MintingRegistrar(storageService, 30)).maybeTryRegister();
 }
 
 const Callbacks = {
---
+++ src/services/MintingRegistrar.ts
@@ -0,0 +1,25 @@
+import ProtocolService from '@services/ProtocolService';
+import {Storage} from '@utils/StorageArray';
+
+export class MintingRegistrar {
+  constructor(private readonly storage: Storage, private readonly statusCheckSleepSeconds: number) {}
+
+  async maybeTryRegister() {
+    const lastCheck = await this.storage.getItem('minting_registrar/last_check');
+    const now = new Date().getTime() / 1000;
+    const shouldCheck = lastCheck == null || now > parseInt(lastCheck) + this.statusCheckSleepSeconds;
+    if (!shouldCheck) return;
+
+    const protocol = await ProtocolService.fromStorage(this.storage);
+    const isRegistered = await protocol.isRegisteredForMinting(protocol.address());
+    if (isRegistered) {
+      console.log('Already registered for next minting, not doing anything');
+    } else {
+      console.log('Not registered for minting, trying to register');
+      await protocol.registerForMinting();
+      console.log(`Successfully registered for minting ${'foo'}`);
+    }
+
+    await this.storage.setItem('minting_registrar/last_check', now.toString());
+  }
+}
