04e5736e514d07a93f0f58cee27bf5cca44da440
Fernando Mendes <fernando@mendes.codes>
2021-08-16 17:47:21 +0100

    Add link to Testnet Terms & Conditions (#89)

--- src/popup/components/Protocol/OptInForm/index.tsx
+++ src/popup/components/Protocol/OptInForm/index.tsx
@@ -11,6 +11,9 @@ import Icon, { IconNames } from "@popup/components/common/Icon";
 import Text, { TextWeights } from "@popup/components/common/Text";
 import Subtitle, { Subsubtitle } from "@popup/components/common/Subtitle";
 
+import WindowsService from "@services/WindowsService";
+import environment from "@environment/index";
+
 const Container = styled.div`
   width: 100%;
   height: 100%;
@@ -96,9 +99,17 @@ function OptInForm() {
 
       <Spacing size="var(--s-16)" />
 
-      <Subsubtitle underline uppercase>
-        Terms & Conditions
-      </Subsubtitle>
+      <Subsubtitle uppercase>By clicking “Opt In” you agree to our</Subsubtitle>
+
+      <a
+        href={`${environment.FRACTAL_WEBSITE_URL}/documents/testnet-launch-special-user-agreement`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Subsubtitle underline uppercase>
+          Special Testnet Launch User Agreement.
+        </Subsubtitle>
+      </a>
 
       <Spacing />
 
