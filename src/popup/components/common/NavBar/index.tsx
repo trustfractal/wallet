d3d8c379dd12d6b7f0acdf1b43bd65810db02f1e
Fernando Mendes <fernando@mendes.codes>
2021-08-16 12:07:06 +0100

    Add balance to navbar

--- src/popup/components/common/NavBar/index.tsx
+++ src/popup/components/common/NavBar/index.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import styled from "styled-components";
+import type { AccountData, Balance } from "@polkadot/types/interfaces";
 
 import {
   useAppDispatch,
@@ -9,7 +10,10 @@ import {
 import appActions from "@redux/stores/application/reducers/app";
 import { useUserSelector } from "@redux/stores/user/context";
 
-import { isSetup } from "@redux/stores/application/reducers/app/selectors";
+import {
+  getProtocolOptIn,
+  isSetup,
+} from "@redux/stores/application/reducers/app/selectors";
 
 import { getCredentials } from "@redux/stores/user/reducers/credentials/selectors";
 import Logo, { LogoSizes } from "@popup/components/common/Logo";
@@ -24,6 +28,12 @@ import Menu from "@popup/components/common/Menu";
 import { exportFile } from "@utils/FileUtils";
 
 import RoutesPaths from "@popup/routes/paths";
+import { ProtocolProvider, useProtocol } from "@services/ProtocolService";
+import {
+  getRegistrationState,
+  getWallet,
+} from "@redux/stores/user/reducers/protocol/selectors";
+import { protocolRegistrationTypes } from "@redux/stores/user/reducers/protocol";
 
 const NavbarContainer = styled.div`
   display: flex;
@@ -48,6 +58,49 @@ const RootContainer = styled.div`
 
   min-width: 400px;
   min-height: 460px;
+`;
+
+const BalanceContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const BalanceTitleContainer = styled.div`
+  color: var(--c-orange);
+`;
+
+const BalanceTextContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  align-items: flex-end;
+`;
+
+const BalanceFreeContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  margin-right: var(--s-10);
+`;
+
+const BalanceReservedContainer = styled.span`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+`;
+
+const BalanceFree = styled.span`
+  margin-right: var(--s-3);
+`;
+
+const BalanceReserved = styled.span<{ isPositive: boolean }>`
+  color: ${(props) => (props.isPositive ? "var(--c-green)" : "var(--c-red)")};
+  margin-right: var(--s-3);
+`;
+
+const BalanceReservedLabel = styled.span`
+  opacity: 0.6;
 `;
 
 function MenuNavbar() {
@@ -92,6 +145,110 @@ function MenuNavbar() {
   );
 }
 
+function ProtocolReservedBalance({ reserved }: { reserved: Balance }) {
+  if (reserved.toNumber() === 0) return <></>;
+
+  const isPositive = reserved.toNumber() > 0;
+
+  return (
+    <BalanceReservedContainer>
+      <BalanceReserved isPositive={isPositive}>
+        <Text
+          size={TextSizes.MEDIUM}
+          height={TextHeights.MEDIUM}
+          weight={TextWeights.BOLD}
+        >
+          {isPositive ? "+" : "-"}
+          {reserved.toNumber()}
+        </Text>
+      </BalanceReserved>
+
+      <BalanceReservedLabel>
+        <Text size={TextSizes.SMALL} height={TextHeights.SMALL}>
+          EXPECTED INCREASE
+        </Text>
+      </BalanceReservedLabel>
+    </BalanceReservedContainer>
+  );
+}
+
+function ProtocolBalance({ balance }: { balance?: AccountData }) {
+  if (!balance)
+    return (
+      <BalanceContainer>
+        <BalanceTitleContainer>
+          <Text
+            size={TextSizes.SMALL}
+            height={TextHeights.SMALL}
+            weight={TextWeights.BOLD}
+          >
+            BALANCE
+          </Text>
+        </BalanceTitleContainer>
+        <BalanceTextContainer>Fetching balance...</BalanceTextContainer>
+      </BalanceContainer>
+    );
+
+  return (
+    <BalanceContainer>
+      <BalanceTitleContainer>
+        <Text
+          size={TextSizes.SMALL}
+          height={TextHeights.SMALL}
+          weight={TextWeights.BOLD}
+        >
+          BALANCE
+        </Text>
+      </BalanceTitleContainer>
+      <BalanceTextContainer>
+        <BalanceFreeContainer>
+          <BalanceFree>
+            <Text
+              size={TextSizes.MEDIUM}
+              height={TextHeights.MEDIUM}
+              weight={TextWeights.BOLD}
+            >
+              {balance.free.toNumber()}
+            </Text>
+          </BalanceFree>
+
+          <Text size={TextSizes.SMALL} height={TextHeights.SMALL}>
+            FCL
+          </Text>
+        </BalanceFreeContainer>
+        <ProtocolReservedBalance reserved={balance.reserved} />
+      </BalanceTextContainer>
+    </BalanceContainer>
+  );
+}
+
+function ProtocolNavbar() {
+  const [balance, setBalance] = useState<AccountData>();
+  const wallet = useUserSelector(getWallet);
+  const protocol = useProtocol();
+
+  useEffect(() => {
+    if (!protocol || !wallet) return;
+
+    const fetchBalance = async () => {
+      const accountBalance = await protocol.getBalance(wallet!.address);
+      setBalance(accountBalance);
+    };
+
+    fetchBalance();
+  }, [protocol, wallet]);
+
+  return (
+    <NavbarContainer>
+      <LogoContainer>
+        <Logo size={LogoSizes.SMALL} />
+      </LogoContainer>
+
+      <ProtocolBalance balance={balance} />
+    </NavbarContainer>
+  );
+}
+
 function LogoNavbar() {
   return (
     <NavbarContainer>
@@ -111,6 +268,19 @@ function LogoNavbar() {
 
 export default function Navbar() {
   const setup = useAppSelector(isSetup);
+  const protocolOptIn = useAppSelector(getProtocolOptIn);
+  const registrationState = useUserSelector(getRegistrationState);
+
+  if (
+    protocolOptIn &&
+    registrationState === protocolRegistrationTypes.COMPLETED
+  ) {
+    return (
+      <ProtocolProvider>
+        <ProtocolNavbar />
+      </ProtocolProvider>
+    );
+  }
 
   if (setup) {
     return <MenuNavbar />;
