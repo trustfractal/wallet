1b50ae30022445c231ed8cc01a73c2f96158e181
Diogo Couto <diogo2couto@gmail.com>
2021-09-14 16:01:20 +0100

    Fix link button when user has no KYCs (#108)

--- src/types/plugin/Environment.ts
+++ src/types/plugin/Environment.ts
@@ -3,6 +3,7 @@ export type Environment = {
   GOLDFISH_URL: string;
   FRACTAL_WEBSITE_URL: string;
   MAGURO_URL: string;
+  JOURNEY_URL: string;
   IS_DEV: boolean;
   PROTOCOL_RPC_ENDPOINT: string;
   PROTOCOL_CURRENCY: string;
