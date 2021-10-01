94e12382e8cea53c03e3bb02d3079f8b4f3d8a4b
Shelby Doolittle <shelby@shelbyd.com>
2021-10-01 06:31:34 -0400

    show connect to fractal if not connected in protocol flow

--- src/popup/components/Protocol/NoLiveness.tsx
+++ src/popup/components/Protocol/NoLiveness.tsx
@@ -7,20 +7,36 @@ import {
   VerticalSequence,
 } from "@popup/components/Protocol/common";
 
+import { getFractalAccountConnector } from "@services/Factory";
+
 import environment from "@environment/index";
 
 export function NoLiveness({ onClick }: { onClick: () => void }) {
-  return (
-    <VerticalSequence>
-      <Icon name={IconNames.PROTOCOL} />
-
-      <Title>You haven’t verified your identity yet</Title>
+  const nextStep = getFractalAccountConnector().hasConnectedAccount() ? (
+    <>
       <BoldText>
         To earn {environment.PROTOCOL_CURRENCY}, start by providing a valid
         liveness.
       </BoldText>
 
       <Cta onClick={onClick}>Verify Identity</Cta>
+    </>
+  ) : (
+    <>
+      <BoldText>
+        You haven't connected your Fractal Account to the extension.
+      </BoldText>
+      <Cta onClick={() => getFractalAccountConnector().doConnect()}>
+        Connect Account
+      </Cta>
+    </>
+  );
+  return (
+    <VerticalSequence>
+      <Icon name={IconNames.PROTOCOL} />
+
+      <Title>You haven’t verified your identity yet</Title>
+      {nextStep}
     </VerticalSequence>
   );
 }
