3e76132628ea50fa2a65af858ff2942c80dff14c
Shelby Doolittle <shelby@shelbyd.com>
2021-10-09 01:35:39 -0400

    Show message when passwords do not match

--- src/popup/components/Login/index.tsx
+++ src/popup/components/Login/index.tsx
@@ -91,6 +91,7 @@ function Login(props: LoginProps) {
             error={error}
             onChange={onChangePassword}
             onEnter={onClick}
+            autoFocus
           />
         </InputContainer>
         <ActionsContainer>
