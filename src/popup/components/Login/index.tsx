a957117b997a5602bee681e64b779167adfc7eea
Shelby Doolittle <shelby@shelbyd.com>
2021-10-09 02:14:41 -0400

    change how login screen shows errors

--- src/popup/components/Login/index.tsx
+++ src/popup/components/Login/index.tsx
@@ -44,34 +44,23 @@ const InputContainer = styled.div`
 type LoginProps = {
   loading: boolean;
   onNext: (password: string) => void;
-  error: string;
+  error?: string;
 };
 
 function Login(props: LoginProps) {
-  const { loading, onNext, error: propError } = props;
+  const { loading, onNext, error } = props;
 
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(propError);
+  const [clearError, setClearError] = useState(true);
+
+  const valid = password.length > 0 && (clearError || error == null);
 
   const onClick = () => {
-    if (loading || isPasswordEmpty || hasError) {
-      return;
-    }
-
+    if (loading || !valid) return;
     onNext(password);
   };
 
-  const hasError = error.length > 0;
-  const isPasswordEmpty = password.length === 0;
-
-  const onChangePassword = ({
-    target: { value },
-  }: React.ChangeEvent<HTMLInputElement>) => {
-    setError("");
-    setPassword(value);
-  };
-
-  useEffect(() => setError(propError), [propError]);
+  useEffect(() => setClearError(error == null), [error]);
 
   return (
     <TopComponent>
@@ -88,18 +77,17 @@ function Login(props: LoginProps) {
             name="value"
             label="Enter your password"
             value={password}
-            error={error}
-            onChange={onChangePassword}
+            error={clearError ? undefined : error}
+            onChange={(event) => {
+              setClearError(true);
+              setPassword(event.target.value);
+            }}
             onEnter={onClick}
             autoFocus
           />
         </InputContainer>
         <ActionsContainer>
-          <Button
-            onClick={onClick}
-            loading={loading}
-            disabled={isPasswordEmpty || hasError}
-          >
+          <Button onClick={onClick} loading={loading} disabled={!valid}>
             Unlock my wallet
           </Button>
         </ActionsContainer>
